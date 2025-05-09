3b391a36cbed6675d8d3cb73a4f6a41bd2f5cb22
me <me@DESKTOP-N3KKTDO>
2025-05-09 23:29:23 +0900

    [JCM] enterprise List 구현

--- ks54team01/src/main/java/ks54team01/admin/enterprise/domain/AdminEntList.java
+++ ks54team01/src/main/java/ks54team01/admin/enterprise/domain/AdminEntList.java
@@ -1,9 +1,19 @@
 package ks54team01.admin.enterprise.domain;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class AdminEntList {
 	
+	private String ceoCode;
 	private String entId;
+	private String entName;
+	private String entBrno;
+	private String ceoTelNo;
+	private String entContractStatus;
+	private LocalDate contractEndDate;
+
 }
+
