ced6f517da4a5c733294625bbbd1c57e3c7f82b2
js-woo861125 <js-woo@nate.com>
2025-05-15 17:55:15 +0900

    [jsw] admin 계약관리 수정

--- ks54team01/src/main/java/ks54team01/admin/contract/service/AdminContractService.java
+++ ks54team01/src/main/java/ks54team01/admin/contract/service/AdminContractService.java
@@ -3,9 +3,12 @@ package ks54team01.admin.contract.service;
 import java.util.List;
 
 import ks54team01.admin.contract.domain.AdminContract;
+import ks54team01.admin.contract.domain.AdminContractDetail;
 
 public interface AdminContractService {
-
+	
+	List<AdminContractDetail> getContractDetail(String rentalContNo);
+	
 	List<AdminContract> getSearchContractList(String searchKey, String searchValue);
 	
 	List<AdminContract> getContractList();
