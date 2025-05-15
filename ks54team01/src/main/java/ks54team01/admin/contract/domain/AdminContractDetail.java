81d35a2586f305eccaae1d19a4b4c4fca6dcedd9
구슬이 <@DESKTOP-4QF1I11>
2025-05-16 00:08:40 +0900

    [jsw] 계약조회페이지 팝업

--- ks54team01/src/main/java/ks54team01/admin/contract/domain/AdminContractDetail.java
+++ ks54team01/src/main/java/ks54team01/admin/contract/domain/AdminContractDetail.java
@@ -5,18 +5,26 @@ import lombok.Data;
 @Data
 public class AdminContractDetail {
 
+	// 0 계약번호상세
 	private String rentalContractDetailNum;
+	// 1 납입회차
 	private int paidInRound;
+	// 2 월납입금액
 	private String paymentAmount;
+	// 3 위약금
 	private String penaltyFee;
+	// 4 월별납입
 	private String monthlyDate;
+	// 5 납입예정일
 	private String monthlyPaymentDate;
+	// 6 납입여부
 	private String unpaidStatus;
+	// 7 계약유형
 	private String rentalContType;
+	// 0 계약번호
 	private String rentalContNo;
 	
 	
-
 	
 
 
