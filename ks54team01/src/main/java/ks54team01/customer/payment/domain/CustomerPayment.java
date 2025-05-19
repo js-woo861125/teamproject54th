9c8f85c6b92c568658bc9626c8ba8a337be1f9de
imwjddyd <imwjddyd@naver.com>
2025-05-19 23:59:32 +0900

    [CJY] 결제 payment 테이블 등록, paymentList 조회 기능 추가

--- ks54team01/src/main/java/ks54team01/customer/payment/domain/CustomerPayment.java
+++ ks54team01/src/main/java/ks54team01/customer/payment/domain/CustomerPayment.java
@@ -1,5 +1,29 @@
 package ks54team01.customer.payment.domain;
 
-public class CustomerPayment {
+import lombok.Data;
 
+@Data
+public class CustomerPayment {
+	
+	private String paymentCompletedNo;
+	private String custId;
+	private String sellProdNo;
+	private String rentalContractNo;
+	private String paymentInfoNo;
+	private String entCeoNo;
+	private String entEmpNo;
+	private String managerId;
+	private String paymentType;
+	private String contractDetail;
+	private String paymentDetail;
+	private Integer paymentCount;
+	private Integer prodUnitPrice;
+	private Integer totalPrice;
+	private String paymentCompletedDate;
+	private String paymentStatus;
+	private String revDate;
+	
+	private String quantity;
+	
+	private String prodNm;
 }
