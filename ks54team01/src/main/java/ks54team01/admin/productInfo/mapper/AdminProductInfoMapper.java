94264489f4c1bbad3623f49a6d8e2410355eb22a
iamchaeyoung <cpfl0317@naver.com>
2025-05-19 17:42:26 +0900

    [KCY] 상품정보관리 수정 완료, 중복체크 ing

--- ks54team01/src/main/java/ks54team01/admin/productInfo/mapper/AdminProductInfoMapper.java
+++ ks54team01/src/main/java/ks54team01/admin/productInfo/mapper/AdminProductInfoMapper.java
@@ -19,123 +19,114 @@ public interface AdminProductInfoMapper {
 	/**
 	 * 상품정보 검색 
 	 */
-	
-	// 상품정보 카테고리별/상세스펙 검색
+	// 모델별/상세스펙 검색
+	List<ProductInfoModelSpec> getSearchModelSpec(String searchKey, String searchValue);
+	// 카테고리별/상세스펙 검색
 	List<ProductInfoCategorySpec> getSearchCategorySpec(String searchKey, String searchValue);
-	// 상품정보 전체혜택 검색
+	// 전체혜택 검색
 	List<ProductInfoBenefit> getSearchBenefit(String searchKey, String searchValue);
-	// 상품정보 모델 검색
+	// 모델 검색
 	List<ProductInfoModel> getSearchModel(String searchKey, String searchValue);
-	// 상품정보 품목 검색
+	// 품목 검색
 	List<ProductInfoItem> getSearchItem(String searchKey, String searchValue);
-	// 상품정보 브랜드 검색
+	// 브랜드 검색
 	List<ProductInfoBrand> getSearchBrand(String searchKey, String searchValue);
-	// 상품정보 카테고리 검색
+	// 카테고리 검색
 	List<ProductInfoCategory> getSearchCategory(String searchKey, String searchValue);
 	
 	/**
 	 * 상품정보 삭제
 	 */
-	// 상품정보 브랜드 삭제
+	// 브랜드 삭제
 	int removeBrandInfoByNo(String brandNo);
 	
 	/**
 	 * 상품정보 중복체크
 	 */
-	// 상품정보 카테고리+스펙명 중복체크
+	// 모델+스펙+상세스펙내용 중복체크
+	boolean isSpecContentCheck(Map<String, Object> params);
+	// 카테고리+스펙명 중복체크
 	boolean isSpecNameCheck(Map<String, Object> params);
-	// 상품정보 혜택명 중복체크
+	// 혜택명 중복체크
 	boolean isBenefitNameCheck (String benefitName);
-	// 상품정보 카테고리+품목명 중복체크
+	// 카테고리+브랜드+품목+모델명 중복체크
+	boolean isModelNameCheck (Map<String, Object> params);
+	// 카테고리+품목명 중복체크
 	boolean isItemNameCheck(Map<String, Object> params);
-	// 상품정보 브랜드명 중복체크
+	// 브랜드명 중복체크
 	boolean isBrandNameCheck (String brandName);
+	// 대분류+중분류+소분류 중복체크
+	boolean isCategoryCheck (Map<String, Object> params);
 	
 	/**
 	 * 상품정보 수정
 	 */
-	// 상품정보 카테고리별/상세스펙 수정
+	// 모델별/상세스펙 수정
+	int modifyModelSpec(ProductInfoModelSpec productInfoModelSpec);
+	// 카테고리별/상세스펙 수정
 	int modifyCategorySpec(ProductInfoCategorySpec productInfoCategorySpec);
-	
-	// 상품정보 전체혜택 수정
+	// 전체혜택 수정
 	int modifyBenefit(ProductInfoBenefit productInfoBenefit);
-	
-	// 상품정보 모델 수정
+	// 모델 수정
 	int modifyModel(ProductInfoModel productInfoModel);
-	
-	// 상품정보 품목 수정
+	// 품목 수정
 	int modifyItem(ProductInfoItem productInfoItem);
-	
-	// 상품정보 브랜드 수정
+	// 브랜드 수정
 	int modifyBrand(ProductInfoBrand productInfoBrand);
-	
-	// 상품정보 카테고리 수정
+	// 카테고리 수정
 	int modifyCategory(ProductInfoCategory productInfoCategory);
 	
 	/**
 	 * 상품정보 조회
 	 */
-	// 상품정보 카테고리별/상세스펙 조회
+	// 모델별/상세스펙 조회
+	ProductInfoModelSpec getModelSpecInfoByNo(String modelSpecNo);
+	// 카테고리별/상세스펙 조회
 	ProductInfoCategorySpec getCategorySpecInfoByNo(String categorySpecyNo);
-	
-	// 상품정보 전체혜택 조회
+	// 전체혜택 조회
 	ProductInfoBenefit getBenefitInfoByNo(String benefitNo);
-	
-	// 상품정보 모델 조회
+	// 모델 조회
 	ProductInfoModel getModelInfoByNo(String modelNo);
-	
-	// 상품정보 품목 조회
+	// 품목 조회
 	ProductInfoItem getItemInfoByNo(String itemNo);
-	
-	// 상품정보 브랜드 조회
+	// 브랜드 조회
 	ProductInfoBrand getBrandInfoByNo(String brandNo);
-	
-	// 상품정보 카테고리 조회
+	// 카테고리 조회
 	ProductInfoCategory getCategoryInfoByNo(String categoryNo);
 	
 	/**
 	 * 상품정보 등록
 	 */
-	// 상품정보 카테고리별/상세스펙 등록
+	// 모델별/상세스펙 등록
+	int addModelSpec(ProductInfoModelSpec productInfoModelSpec);
+	// 카테고리별/상세스펙 등록
 	int addCategorySpec(ProductInfoCategorySpec ProductInfoCategorySpec);
-	
-	// 상품정보 전체혜택 등록
+	// 전체혜택 등록
 	int addBenefit(ProductInfoBenefit productInfoBenefit);
-	
-	// 상품정보 모델 등록
+	// 모델 등록
 	int addModel(ProductInfoModel productInfoModel);
-	
-	// 상품정보 품목 등록
+	// 품목 등록
 	int addItem(ProductInfoItem productInfoItem);
-	
-	// 상품정보 브랜드 등록
+	// 브랜드 등록
 	int addBrand(ProductInfoBrand productInfoBrand);
-	
-	// 상품정보 카테고리 등록
+	// 카테고리 등록
 	int addCategory(ProductInfoCategory productInfoCategory);
 	
 	/**
 	 * 상품정보 목록 조회
 	 */
-	// 상품정보 모델별/상세스펙 목록 조회
+	// 모델별/상세스펙 목록 조회
 	List<ProductInfoModelSpec> getModelSpecList();
-	
-	// 상품정보 카테고리별/상세스펙 목록 조회
+	// 카테고리별/상세스펙 목록 조회
 	List<ProductInfoCategorySpec> getCategorySpecList();
-	
-	// 상품정보 전체혜택 목록 조회
+	// 전체혜택 목록 조회
 	List<ProductInfoBenefit> getBenefitList();
-	
-	// 상품정보 모델 목록 조회
+	// 모델 목록 조회
 	List<ProductInfoModel> getModelList();
-	
-	// 상품정보 품목 목록 조회
+	// 품목 목록 조회
 	List<ProductInfoItem> getItemList();
-	
-	// 상품정보 브랜드 목록 조회
+	// 브랜드 목록 조회
 	List<ProductInfoBrand> getBrandList();
-	
-	// 상품정보 카테고리 목록 조회
+	// 카테고리 목록 조회
 	List<ProductInfoCategory> getCategoryList();
-
 }
