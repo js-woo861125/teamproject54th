96bd7766b34f384ee1aff1a60ee5731cf1dfb20a
soneunhyang <snh2520@naver.com>
2025-05-23 17:24:01 +0900

    []

--- ks54team01/src/main/java/ks54team01/admin/login/service/AdminLoginService.java
+++
@@ -1,8 +0,0 @@
-package ks54team01.admin.login.service;
-
-import java.util.Map;
-
-public interface AdminLoginService {
-	// 회원정보 조회
-	Map<String, Object> matchMember(String memberId, String memberPw);
-}
--- ks54team01/src/main/java/ks54team01/admin/mypage/service/AdminMypageService.java
+++
@@ -1,5 +0,0 @@
-package ks54team01.admin.mypage.service;
-
-public interface AdminMypageService {
-
-}
