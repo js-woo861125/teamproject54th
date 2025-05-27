ad264f69ca5ac323038ba6b55dc18dfaf272b382
js-woo861125 <js-woo@nate.com>
2025-05-27 17:22:46 +0900

    [jsw] 정산페이지 기능추가... 살려줘 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

---
+++ ks54team01/src/main/java/ks54team01/admin/payment/service/AdminFeeService.java
@@ -0,0 +1,9 @@
+package ks54team01.admin.payment.service;
+
+import ks54team01.admin.payment.domain.SettlementConfirmRequest;
+
+public interface AdminFeeService {
+
+	 void saveSettlement(SettlementConfirmRequest request);
+	
+}
