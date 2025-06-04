dee96a862156c19dd08aee6b413de09e32a5fd93
imwjddyd <imwjddyd@naver.com>
2025-06-04 17:25:08 +0900

    [CJY] 상품상세 개월별 혜택 표시 구현
    Merge branch 'develop' into CJY

---
+++ ks54team01/src/main/java/ks54team01/customer/product/domain/CustomerProductBenefit.java
@@ -0,0 +1,17 @@
+package ks54team01.customer.product.domain;
+
+import lombok.Data;
+
+@Data
+public class CustomerProductBenefit {
+
+	private String prodBenefitNo;
+	private String sellProdNo;
+	private String entCeoNo;
+	private String entEmpId;
+	private String benefitNo;
+	private String benefitDetail;
+	private String regDate;
+	private String revDate;
+	private String useStatus;
+}
