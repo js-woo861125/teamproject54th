d44938ae7dcff7f56493f5ad8bd5b4fad8bdaa75
user <user@DESKTOP-66NMTNO>
2025-06-08 20:31:34 +0900

    [PJS] - 최종 로직 점검, 레이아웃 구조 수정

--- ks54team01/src/main/java/ks54team01/customer/transferBoard/controller/CustomerTransferBoardController.java
+++ ks54team01/src/main/java/ks54team01/customer/transferBoard/controller/CustomerTransferBoardController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
